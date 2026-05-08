9db7c6edd6a7fcf543eb2f475c8e1195c824d329
Merve <mervebzkurt34@gmail.com>
2026-05-08 18:07:15 +0300

    first commit

---
+++ hesap_makinesi.py
@@ -0,0 +1,9 @@
+                                                                       
+x=float( input("İlk sayıyı giriniz.:" ))
+y=float(input("İkinci sayıyı giriniz.:" ))
+print("Girilen sayılar:", x,y)
+print("---SONUÇLAR---")
+print("Toplam: ", x+y)
+print("Fark: ", x-y)
+print("Çarpım: ", x*y)
+print("Bölüm: " , x/y)
